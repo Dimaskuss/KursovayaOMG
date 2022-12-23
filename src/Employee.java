a16bd9033345269e63817692e888f78111b95b83
Дмитрий <dimadrumfire@gmail.com>
2022-12-23 23:26:17 +0600

    method one done !!

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,74 @@
+import java.util.Objects;
+
 public class Employee {
 
-}
+    private final String name;
+    private int numberDepartment;
+    private int salary;
+    private final int id;
+    private static int counter;
+
+
+    public Employee(String name, int numberDepartment, int salary) {
+        if (numberDepartment < 0 || numberDepartment > 5) {
+            throw new IllegalArgumentException("Такого отдела не существует");
+        }
+        this.name = name;
+        this.numberDepartment = numberDepartment;
+        this.salary = salary;
+        counter++;
+        this.id = counter;
+
+    }
+
+    private int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberDepartment() {
+        return numberDepartment;
+    }
+
+    public  int getSalary() {
+        return salary;
+    }
+
+    public void setNumberDepartment(int numberDepartment) {
+        this.numberDepartment = numberDepartment;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+     @Override
+    public  String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", numberDepartment=" + numberDepartment +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+
+    }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.OptionalDataException;
+
 public class Main {
+
+    static Employee employees[] = new Employee[10];
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        employees[0] = new Employee("Дима", 1, 99000);
+        employees[1] = new Employee("Елена", 3, 43000);
+        employees[2] = new Employee("Максим", 2, 39000);
+        employees[3] = new Employee("Денис", 3, 54000);
+        employees[4] = new Employee("Валенитин", 2, 62000);
+        employees[5] = new Employee("Захар", 4, 31000);
+        employees[6] = new Employee("Лидия", 3, 76000);
+        employees[7] = new Employee("Ярослав", 2, 62000);
+       employees[8] = new Employee("Никита", 4, 59000);
+        employees[9] = new Employee("Дарина", 3, 48000);
+
+
+
+
+    EmployeeService.printEmployee();
     }
 }
