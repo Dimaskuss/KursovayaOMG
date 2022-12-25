da722e9c1df2f3a5e66690851f3f1817cb60a0c6
Дмитрий <dimadrumfire@gmail.com>
2022-12-26 01:51:50 +0600

    Increased difficulty !!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         employees[2] = new Employee("Максим", 4, 39001);
         employees[3] = new Employee("Денис", 3, 54000);
         employees[4] = new Employee("Валенитин", 2, 62001);
-        employees[5] = new Employee("Захар", 5, 31000);
+        employees[5] = new Employee("Захар", 4, 31000);
         employees[6] = new Employee("Лидия", 4, 76000);
         employees[7] = new Employee("Ярослав", 2, 62000);
         employees[8] = new Employee("Никита", 4, 59000);
@@ -30,7 +30,9 @@ public class Main {
         getMinSalary();
         getDepartmentSalaryMax(2);
         getDepartmentSalaryMin(2);
+        System.out.println();
         getDepartmentSalarySum(2);
+        System.out.println();
         calculateAvarageDepartmentSalary(2);
         calculateAvarageDepartmentSalary(3);
         indexDepartmentSalary(2, 10.32);
@@ -38,6 +40,24 @@ public class Main {
         printEmployeesSalaryLessThen(50000);
         System.out.println();
         printEmployeesSalaryMoreThen(50000);
+        availableNumberDepartment(5);
+
+
+    }
+//    пробник по наличию людей в отделе
+   public static void availableNumberDepartment(int dep) {
+        int counter=0 ;
+        for (int i = 0; i < employees.length && employees[i] != null ; i++) {
+            if (employees[i].getNumberDepartment() == dep) {
+                counter++;
+            }
+
+            }
+            if (counter==0) {
+                System.out.println("В этом департаменте нет ни одного сотрудника .");
+
+        }
+
     }
 
     public static void printEmployee() {
@@ -112,6 +132,7 @@ public class Main {
         if (departmentId < 0 || departmentId > 5) {
             throw new IllegalArgumentException("Такого отдела не существует");
         }
+
         int maxSalary = Employee.minnn;
         for (int i = 0; i < employees.length && employees[i] != null; i++) {
             if (departmentId == employees[i].getNumberDepartment() && employees[i].getSalary() > maxSalary) {
@@ -124,12 +145,20 @@ public class Main {
     public static void getDepartmentSalaryMin(int departmentId) {
         if (departmentId < 0 || departmentId > 5) {
             throw new IllegalArgumentException("Такого отдела не существует");
+
         }
+        int counter = 0;
         int minSalary = Employee.maxxx;
         for (int i = 0; i < employees.length && employees[i] != null; i++) {
-            if (departmentId == employees[i].getNumberDepartment() && employees[i].getSalary() < minSalary) {
+            if (employees[i].getSalary() < minSalary && departmentId == employees[i].getNumberDepartment()) {
                 minSalary = employees[i].getSalary();
+                counter++;
             }
+            }
+
+        if (counter == 0) {
+            minSalary = counter;
+            System.out.println("В этом отделе нет сотрудников.");
         }
         System.out.println(minSalary);
     }
